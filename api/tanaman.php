3cffce9015c2b3787ccafedcaa7d62cbb1bb8784
Habib Husain Nurrohim <habibsain55@gmail.com>
2024-10-28 10:29:07 +0700

    update put ketika idnya tidak ada

--- api/tanaman.php
+++ api/tanaman.php
@@ -25,7 +25,7 @@
                     );
                     echo respon($respon, 500);
                 die;
-                    
+
                 }
                 else
                 {
@@ -138,15 +138,12 @@
                 $get_body = file_get_contents("php://input"); // jadi data JSON
                 $get_json = json_decode($get_body, TRUE); // jadi array data
 
-                $_POST['id'] = $get_json['id'];
                 $_POST['tanggal'] = $get_json['tanggal'];
                 $_POST['tanaman'] = $get_json['tanaman'];
                 $_POST['keterangan'] = $get_json['keterangan'];
 
-            if($_POST['id'] == '' OR $_POST['tanggal'] == '' OR $_POST['tanaman'] == '' OR $_POST['keterangan'] == '')
+            if($_POST['tanggal'] == '' OR $_POST['tanaman'] == '' OR $_POST['keterangan'] == '')
             {
-              
-        
                 $respon =  array(
                     'status' => false,
                     'message' => 'ID, Tanggal, tanaman, keterangan dan foto tidak boleh kosong !!!',
@@ -159,6 +156,20 @@
             else
             {
                 $get_id = $_GET['id'];
+                $get_edit_tanaman = ambil_data_tanaman($get_id);
+                    if($get_edit_tanaman == false)
+                    {
+                    $respon =  array(
+                        'status' => false,
+                        'message' => 'Tanaman tidak ditemukan',
+                        'data' => array()
+                    );
+            
+                    echo respon($respon, 404);
+                }
+                else
+                {
+                $get_id = $_GET['id'];
                 $get_tanggal = $_POST['tanggal'];
                 $get_tanaman = $_POST['tanaman'];
                 $get_keterangan = $_POST['keterangan'];
@@ -166,7 +177,6 @@
                     $get_gambar = upload_tanaman();
                 } else {
                     // Jika tidak ada file baru, tetap menggunakan gambar lama
-                    $get_edit_tanaman = ambil_data_tanaman($get_id);
                     $get_gambar = $get_edit_tanaman['foto'];
                 }   
                 $update_tanaman = update_tanaman($get_id,$get_tanggal,$get_tanaman,$get_keterangan,$get_gambar);
@@ -192,6 +202,7 @@
                     echo respon($respon, 500);
                 }
             }
+        }
             break;
         default:
 
